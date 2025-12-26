940b66b67f3695ee36995a389459277f755bd204
Roshan Kewat <roshankewat9090@gmail.com>
2025-12-26 13:46:16 +0530

    Minor Fix.

--- backend/src/routes/project-user.routes.ts
+++ backend/src/routes/project-user.routes.ts
@@ -3,13 +3,17 @@ import express from 'express'
 import {
     ProjectUserSignUp,
     ProjectUserVerifyEmail,
-    ProjectUserLogin
+    ProjectUserLogin,
+    ProjectUserUpdate
 } from '../controllers/project-user.controller'
 
 const router = express.Router()
 
 router.post('/signup', ProjectUserSignUp)
+
 router.get('/verify-email', ProjectUserVerifyEmail)
 router.get('/login', ProjectUserLogin)
+
+router.put('/update', ProjectUserUpdate)
 
 export default router;
